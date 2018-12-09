c2e773ba7b4d780b72572236d286d8d3502f1572
dbeard <dbeard@groupon.com>
2018-12-09 13:15:06 -0800

    Stub out arg parsing, implement generic struct reads

--- src/jmo.jl
+++ src/jmo.jl
@@ -4,6 +4,8 @@ module JMO
 include("constants.jl")
 include("types.jl")
 
+using ArgParse
+
 function printHeader64(header)
   h = header
   println("magic\t\tcputype\t\tcpusubtype\tfiletype\tncmds\t\tsizeofcmds\tflags\t\treserved")
@@ -59,30 +61,15 @@ function read_mach_header(f::IOStream, offset, is_64::Bool, is_swap::Bool)
   end
 end
 
-# iostream, offset, is_swap
-function read_load_command(f::IOStream, offset::Int64, is_swap::Bool)
-  T, tfields = LoadCommand, fieldnames(LoadCommand)
+function read_generic(T, f::IOStream, offset::Int64, is_swap::Bool)
   seek(f, offset)
-  return T(
-    is_swap ? bswap(read(f, fieldtype(T, 1))) : read(f, fieldtype(T, 1)),
-    is_swap ? bswap(read(f, fieldtype(T, 2))) : read(f, fieldtype(T, 2)))
-end
-
-# Reads a single SegmentCommand64, or SegmentCommand, based on passed in T
-function read_segment_command(T, f::IOStream, offset::Int64, is_swap::Bool)
-  seek(f, offset)
-  cmd       = is_swap ? bswap(read(f, fieldtype(T, 1))) : read(f, fieldtype(T, 1))
-  cmdsize   = is_swap ? bswap(read(f, fieldtype(T, 2))) : read(f, fieldtype(T, 2))
-  segname   = is_swap ? bswap(read(f, fieldtype(T, 3))) : read(f, fieldtype(T, 3))
-  vmaddr    = is_swap ? bswap(read(f, fieldtype(T, 4))) : read(f, fieldtype(T, 4))
-  vmsize    = is_swap ? bswap(read(f, fieldtype(T, 5))) : read(f, fieldtype(T, 5))
-  fileoff   = is_swap ? bswap(read(f, fieldtype(T, 6))) : read(f, fieldtype(T, 6))
-  filesize  = is_swap ? bswap(read(f, fieldtype(T, 7))) : read(f, fieldtype(T, 7))
-  maxprot   = is_swap ? bswap(read(f, fieldtype(T, 8))) : read(f, fieldtype(T, 8))
-  initprot  = is_swap ? bswap(read(f, fieldtype(T, 9))) : read(f, fieldtype(T, 9))
-  nsects    = is_swap ? bswap(read(f, fieldtype(T, 10))) : read(f, fieldtype(T, 10))
-  flags     = is_swap ? bswap(read(f, fieldtype(T, 11))) : read(f, fieldtype(T, 11))
-  return T(cmd, cmdsize, segname, vmaddr, vmsize, fileoff, filesize, maxprot, initprot, nsects, flags)
+  nfields = fieldcount(T)
+  fields = Any[]
+  for i = 1:nfields
+    field = is_swap ? bswap(read(f, fieldtype(T, i))) : read(f, fieldtype(T, i))
+    push!(fields, field)
+  end
+  return T(fields...)
 end
 
 # iostream, offset, header type, ncmds, is_swap
@@ -90,19 +77,49 @@ end
 # Reads all segment commands
 function read_segment_commands(f::IOStream, load_commands_offset::Int64, ncmds::UInt32, is_swap::Bool)
   actual_offset = load_commands_offset
-  for i = 0:ncmds
-    load_cmd = read_load_command(f, actual_offset, is_swap)
+  for i = 1:ncmds
+    # load_cmd = read_load_command(f, actual_offset, is_swap)
+    load_cmd = read_generic(LoadCommand, f, actual_offset, is_swap)
     
     # Load SegmentCommand && SegmentCommand64 values
     # Since only the types of fields change, the fieldtype's handle that.
     if load_cmd.cmd == LC_SEGMENT_64
-      segment_command = read_segment_command(SegmentCommand64, f, actual_offset, is_swap)
+      segment_command = read_generic(SegmentCommand64, f, actual_offset, is_swap)
       segname_string = String(segment_command.segname)
       println("Segname: $(segname_string)")
+      println("Segsize: $(sizeof(segment_command))")
+      println("nsect: $(segment_command.nsects)")
+      
+      # Read sections for this segment
+      current_section_offset = actual_offset + sizeof(segment_command)
+      for sect = 1:segment_command.nsects
+        section = read_generic(Section64, f, current_section_offset, is_swap)
+        println("Section: $(String(section.sectname)) $(String(section.segname))")
+        
+        # Test printing out contents of __cstring
+        # Probably doesn't handle mutliple strings yet.
+        if occursin("__cstring", String(section.sectname))
+          println("Offset: $(section.offset) $(section.size)")
+          existing_index = position(f)
+          seek(f, section.offset)
+          data = read(f, section.size)
+          println("Data: $data $(String(data))")
+          seek(f, existing_index)
+        end
+        
+        current_section_offset += sizeof(section)
+      end
+
+      
+      # Load section commands here...
+      
     elseif load_cmd.cmd == LC_SEGMENT
-      segment_command = read_segment_command(SegmentCommand, f, actual_offset, is_swap)
+      segment_command = read_generic(SegmentCommand, f, actual_offset, is_swap)
       segname_string = String(segment_command.segname)
       println("Segname: $(segname_string)")
+      println("Segsize: $(sizeof(segment_command))")
+      
+      # Load section commands here...
     end
     actual_offset += load_cmd.cmdsize
   end
@@ -127,11 +144,30 @@ function openFile(filename)
   
   # read segment commands
   load_cmds = read_segment_commands(f, offset, header.ncmds, is_swap)
-  println(load_cmds)
+  # println(load_cmds)
   
   close(f)
 end
 
+function parse_cli_opts(args) 
+  s = ArgParseSettings(description = "MachO object file viewer")
+
+  @add_arg_table s begin
+      "-l"
+        action = :store_true
+        help = "Display header"
+      "--opt2", "-o"         # another option, with short form
+      "file"                 # a positional argument
+  end
+
+  parsed_args = parse_args(s) # the result is a Dict{String,Any}
+  println("Parsed args:")
+  for (key,val) in parsed_args
+      println("  $key  =>  $(repr(val))")
+  end
+end
+
+parse_cli_opts(ARGS)
 openFile("/Users/dbeard/Binary")
 
 end # module
